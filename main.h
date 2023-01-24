b11f950cefe81bc0841105490c203d694f9122f2
ProgressAce <daiyaansallie7@gmail.com>
2023-01-24 09:26:19 -0800

    Add function prototypes for int, unsigned_int, octal and binary functions

--- main.h
+++ main.h
@@ -7,8 +7,11 @@
 int _putchar(char c);
 int _printf(const char *format, ...);
 int p_char(va_list c);
-int p_string(va_list s);
+int p_integer(va_list i);
+int p_string(va_list s)
 int p_binary(va_list b);
+int p_unsigned_int(va_list u);
+int p_octal(va_list o);
 
 /**
  * print_format - Struct print_format
