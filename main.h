44f556cab8d657633862b6f27563b39dfd9ad945
ProgressAce <daiyaansallie7@gmail.com>
2023-01-27 08:13:03 -0800

    Add flag characters function

--- main.h
+++ main.h
@@ -30,6 +30,8 @@ int print_octal(va_list o);
 int print_x(va_list x);
 int print_X(va_list X);
 int print_rot13(va_list R);
+int get_flags(const char *format, int i*);
+
 /**
  * print_format - Struct print_format
  *
