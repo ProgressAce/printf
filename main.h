61f8448130d3b2bf5f32149ffd9ffaadad7ff298
ProgressAce <daiyaansallie7@gmail.com>
2023-01-27 08:57:45 -0800

    Remove merged function prototypes

--- main.h
+++ main.h
@@ -17,20 +17,10 @@ int p_unsigned_int(va_list u);
 int p_octal(va_list o);
 int p_x(va_list x);
 int p_X(va_list X);
-
-int _putchar(char c);
-int _printf(const char *format, ...);
-int print_char(va_list c);
-int print_string(va_list s);
-int d(va_list i);
-int print_dec(va_list d);
-int print_bin(va_list b);
-int print_unsig(va_list u);
-int print_octal(va_list o);
-int print_x(va_list x);
-int print_X(va_list X);
+int p_rev(va_list r)
 int print_rot13(va_list R);
 int get_flags(const char *format, int i*);
+
 
 /**
  * print_format - Struct print_format
